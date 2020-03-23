fd15fa7d9eb35fd649b7e6e3a6f2e1448820024f
Diogo Fernandes <diogo.fernandes@lb2.com.br>
2020-03-23 12:54:09 -0300

    fix(form): adicionado campos

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,31 +5,61 @@
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
         <div class="mb-12 xl:mb-0">
-          <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
+          <h4 v-if="isSignedUp">Obrigado, logo entraremos em contato.</h4>
 
           <form
             v-else
             @submit.prevent="handleSubmit"
-            name="signups"
+            name="faleConosco"
             netlify
             class="flex items-center border-b border-b-2 border-blue-400 py-2"
           >
-            <input
-              ref="emailInput"
-              v-model="form.email"
-              class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-              type="text"
-              name="email"
-              placeholder="your@email.com"
-              aria-label="Email address"
-            />
-
-            <button
-              class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
-              type="submit"
-            >
-              Sign Up
-            </button>
+            <div class="flex flex-wrap mb-6">
+              <div class="w-full px-3">
+                <div class="font-bold text-xl mb-2  text-gray-700">Fale conosco:</div>
+                <label class="block">
+                  <span class="text-gray-700">Nome</span>
+                  <input
+                    class="appearance-none w-full text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded"
+                    id="grid-first-name"
+                    type="text"
+                    placeholder="Nome"
+                  />
+                </label>
+              </div>
+              <div class="w-full px-3">
+                <label class="block">
+                  <span class="text-gray-700">Email</span>
+                  <input
+                    ref="emailInput"
+                    v-model="form.email"
+                    class="appearance-none mb-36 bg-transparent border w-full text-gray-700 py-3 px-4 mb-3 leading-tight focus:outline-none rounded"
+                    type="text"
+                    name="email"
+                    placeholder="nome@email.com.br"
+                    aria-label="Email"
+                  />
+                </label>
+              </div>
+              <div class="w-full px-3">
+                <label class="block">
+                  <span class="text-gray-700">Mensagem</span>
+                  <textarea
+                    class="form-textarea mt-1 block w-full focus:outline-none"
+                    rows="3"
+                    placeholder="Como podemos ajudar?"
+                  ></textarea>
+                </label>
+              </div>
+              <div class="w-full px-3">
+                <button
+                  class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded mt-5 rounded"
+                  type="submit"
+                >
+                  Enviar
+                </button>
+              </div>
+            </div>
           </form>
         </div>
       </div>
@@ -85,7 +115,7 @@ export default class Home extends Vue {
       await fetch('/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: this.encode({ 'form-name': 'signups', ...this.form }),
+        body: this.encode({ 'form-name': 'faleConosco', ...this.form }),
       });
 
       this.isSignedUp = true;
